f66580cfb08dbc21e67801ec272851f8baa7ac5b
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-01-11 21:36:07 +0500

    Install React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Hero from "./components/hero/Hero";
+import About from "./components/about/About";
+import Bloger from "./components/blog/Bloger";
+import CommercialRefergeration from "./components/commercial-refergeration/CommercialRefergeation";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import RepairMaintenance from "./components/repair-maintenance/RepairMaintenance";
+import Testimonials from "./components/testimonials/Testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Justcold</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="about" element={<About />} />
+          <Route path="bloger" element={<Bloger />} />
+          <Route path="commercial-rerergeration" element={<CommercialRefergeration />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="footer" element={<Footer />} />
+          <Route path="header" element={<Header />} />
+          <Route path="repair-maintenance" element={<RepairMaintenance />} />
+          <Route path="testimonials" element={<Testimonials />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/blog/Blog.js
+++
@@ -1,9 +0,0 @@
-import './blog.css';
-
-const Blog = () => {
-    return (
-        <h1>Blog</h1>
-    )
-}
-
-export default Blog;
