675deb45c5f589baa31566902a5699763681bf24
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-01-13 17:01:00 +0500

    Adding font family

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,9 +1,88 @@
+import React, { useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { Link } from 'react-router-dom';
+import IMG from '../../assets/banner.jpg';
 import './hero.css';
 
 const Hero = () => {
-    return (
-        <h1>Hero</h1>
-    )
-}
+  const rightAnimition = useRef();
+  const leftAnimation = useRef();
+
+  useLayoutEffect(() => {
+    const bannerAninmation = gsap.context(() => {
+      gsap.from('.bannerMsgBtnleft', {
+        duration: 1,
+        x: '-600%',
+        ease: '',
+      });
+      gsap.to(leftAnimation.current, {
+        duration: 1,
+        x: '0%',
+        ease: '',
+      });
+      gsap.from('.bannerMsgBtnright', {
+        duration: 1,
+        x: '400%',
+        ease: '',
+      });
+      gsap.to(rightAnimition.current, {
+        duration: 1,
+        x: '0%',
+        ease: '',
+      });
+      gsap.from('.bannerUpAnimation', {
+        duration: 1,
+        y: '-400%',
+        ease: '',
+      });
+      gsap.to(rightAnimition.current, {
+        duration: 1,
+        y: '0%',
+        ease: '',
+      });
+    });
+    return () => bannerAninmation.revert();
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+  return (
+    <div className="home">
+      <div className="banner-container">
+        <img src={IMG} alt="banner" className="banner-img" />
+        <div className="overlay" />
+      </div>
+      <div className="banner-msg">
+        <div className="banner-msg-p">
+          <h1 className="bannerUpAnimation" ref={rightAnimition}>
+            <span className='font-family-coiny'>JustCold</span> Refrigeration
+          </h1>
+          <p className="paragraphAminiation" ref={leftAnimation}>
+            Specialist Fridges Installation & Commercial Refrigeration Service
+          </p>
+        </div>
+        <div className="banner-link">
+          <Link
+            to="our-projects"
+            className="bannerMsgBtnleft"
+            ref={leftAnimation}
+            onClick={scrollToTop}
+          >
+            OUR PROJECTS
+          </Link>
+          <Link
+            to="contact-us"
+            className="bannerMsgBtnright"
+            ref={rightAnimition}
+            onClick={scrollToTop}
+          >
+            DISCUSS YOUER PROJECTS
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
