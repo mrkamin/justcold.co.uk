5ab5e9feed9cbdd3b925b434f05d1aca0f788f54
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-01-22 13:27:04 +0500

    Add Commercial-Refergeration component

--- src/components/commercial-refergeration/CommercialRefergeation.js
+++ src/components/commercial-refergeration/CommercialRefergeation.js
@@ -1,9 +1,20 @@
-import './commercialrefergeration.css';
+import IMG from '../../assets/commercial/banner.jpg';
+import RefergrationSectTwo from './CommercialRefergTwo';
+import "./commercialrefergeration.css";
 
 const CommercialRefergeration = () => {
-    return (
-        <h1>CommercialRefergeration</h1>
-    )
-}
+  return (
+    <>
+      <div className="air-cond-banner">
+        <img src={IMG} alt="bannerphoto" />
+        <div className="air-cond-overlay" />
+        <h1 className="air-cond-heading">
+          Commercial Refrigeration Europe
+        </h1>
+      </div>
+      <RefergrationSectTwo />
+    </>
+  );
+};
 
 export default CommercialRefergeration;
