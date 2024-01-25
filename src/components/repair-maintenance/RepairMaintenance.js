ae273ed830290cb2884539c892484297a11a0ed0
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-01-25 21:01:06 +0500

    Add RepairMaintainece components

--- src/components/repair-maintenance/RepairMaintenance.js
+++ src/components/repair-maintenance/RepairMaintenance.js
@@ -1,8 +1,19 @@
+import RepairMaintaince from './RepairMaintainence';
+import IMG from '../../assets/repair/1 copy.jpg'
 import './repairmaintenance.css';
 
 const RepairMaintenance = () => {
     return (
-        <h1>RepairMaintenance</h1>
+        <>
+        <div className="air-cond-banner position-relative text-center">
+          <img src={IMG} alt="bannerphoto" />
+          <div className="air-cond-overlay position-absolute" />
+          <h1 className="air-cond-heading p-0 m-0 position-absolute w-100">
+            Air Conditioning & Commercial Refrigeration Repair and Maintenance
+          </h1>
+        </div>
+        <RepairMaintaince />
+      </>
     )
 }
 
